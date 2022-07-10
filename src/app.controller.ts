7fde43095a4024d32d7cc8a56c3335b2876cefc2
Thinh Tran <thinhrom11@gmail.com>
2022-07-10 18:43:44 +0700

    refactor: change POST method to GET

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Query } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 import { PostQueryDto } from './dto/postQuery.dto';
 
@@ -6,12 +6,12 @@ import { PostQueryDto } from './dto/postQuery.dto';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Post()
+  @Get()
   getPapers(@Query() query: PostQueryDto) {
     return this.appService.getPapers(query.url);
   }
 
-  @Post('/example')
+  @Get('/example')
   getPaperInfos() {
     return this.appService.getPaperInfos(
       'https://scholar.google.com/citations?view_op=view_citation&hl=en&user=0FqthhoAAAAJ&citation_for_view=0FqthhoAAAAJ:M3ejUd6NZC8C',
